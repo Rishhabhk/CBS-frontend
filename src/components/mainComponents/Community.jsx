6a1ffbcf14fc618ae7fe3d4cb5c63e58f59d3cc3
Harshi Agrawal <114180714+harshi706@users.noreply.github.com>
2023-11-04 23:56:11 +0530

    Changes

--- src/components/mainComponents/Community.jsx
+++ src/components/mainComponents/Community.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import './Community.css';
+import on from './on.jpeg'
+import File from './File';
 
 const Community = () => {
   return (
-    <div id='community'>Community</div>
+    <>
+      <div className='main'>
+        <div className='one'>
+        <img className='on' src={on}/>
+        <div className='input'>
+        <input className='post' type='text'
+        placeholder='Start a post' />
+        </div>
+        <div className='media'>
+        <File />
+        </div>
+        </div>
+        <div className='two'>
+          two
+        </div>
+      </div>
+    </>
   )
 }
 
