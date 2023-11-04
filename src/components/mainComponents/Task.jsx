9094ef1c6e7fe63174cf96b8f02ede2eded0723e
Harshi Agrawal <114180714+harshi706@users.noreply.github.com>
2023-11-04 21:39:57 +0530

    changes in task.jsx

--- src/components/mainComponents/Task.jsx
+++ src/components/mainComponents/Task.jsx
@@ -1,56 +1,54 @@
 import React from 'react'
 import './Task.css';
 import { AiOutlineCheck } from "react-icons/ai";
+import HotsBlogs from './HotsBlogs';
 
 const Task = () => {
-  const[form,setForm]=React.useState({
-    title:"",
-    description:""
+  const [form, setForm] = React.useState({
+    title: "",
+    description: ""
   });
-  function handleform(event){
-    setForm(prev=>{
-      return{
+  function handleform(event) {
+    setForm(prev => {
+      return {
         ...prev,
-        [event.target.name]:event.target.value
+        [event.target.name]: event.target.value
       }
     })
   }
-  function handlefunc(event){
+  function handlefunc(event) {
     event.preventDefault()
   }
   return (
     <>
-    <div className='taskwrapper'>
-      <div className='taskone'>
-        <form onSubmit={handlefunc}>
-          Title<input className='title' type='text'
-          placeholder='Title'
-          name='title'
-          onChange={handleform}
-          value={form.title} />
-           Description<input className='desc' type='text'
-          placeholder='Description'
-          name='description'
-          onChange={handleform}
-          value={form.description} />
-        </form>
-        <br></br>
-        <button className='submit' type='submit'>Add task</button>
-      </div>
+      <div className='taskwrapper'>
+        <div className='taskone'>
+          <form onSubmit={handlefunc}>
+            Title<input className='title' type='text'
+              placeholder='Title'
+              name='title'
+              onChange={handleform}
+              value={form.title} />
+            Description<input className='desc' type='text'
+              placeholder='Description'
+              name='description'
+              onChange={handleform}
+              value={form.description} />
+            <br></br>
+            <button className='submit' type='submit'>Add task</button>
+          </form>
+          <div className='taskthree'>
+            <h2>Tasks</h2>
+            <p className='tasks'>tasks</p>
+            <div className='icon'> <p className='icons'><AiOutlineCheck />Completed</p>
+              <p className='icons'>Share</p></div>
+          </div>
+        </div>
       <div className='tasktwo'>
-      Trending Blogs
+        <HotsBlogs />
       </div>
-      <div className='taskthree'>
-        <h2>Tasks</h2>
-        <p className='tasks'>tasks</p>
-        <div className='icon'> <p className='icons'><AiOutlineCheck />Completed</p>
-        <p className='icons'>Share</p></div>
       </div>
-      <div className='taskfour'>
-        Blogs
-      </div>
-    </div>
-    </>  
-    )
+    </>
+  )
 }
 export default Task
